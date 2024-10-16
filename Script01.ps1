e947f2a102b2ee40132646d8945baf586e943f95
Daneil Alvarez Burgo <danielalvarezburgo@gmail.com>
2024-10-16 12:49:19 +0200

    0.1

---
+++ Script01.ps1
@@ -0,0 +1,4 @@
+﻿New-Item -Path C:/prueba -ItemType Directory
+Set-Location -Path C:/prueba
+New-Item -Path . -Name "hola.txt" -ItemType File -Value ("Daniel Alvarez Burgo")
+Copy-Item -Path hola.txt -Destination hola.bak
